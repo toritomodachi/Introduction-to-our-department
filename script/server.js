8de331866bd82fa3762eb05e82f7ab97aef8061a
toritomodachi <tanncheonn@gmail.com>
2023-05-30 12:25:48 +0900

    저장

--- script/server.js
+++ script/server.js
@@ -3,11 +3,12 @@ $(window).on('scroll', function(){
       $('.logo').css('width', '100px');
       $('.logo').css('height','100px');
       $('header').css('height','100px');
-      $()
+      $('header').css('opacity','0.5');
     }
     else{
         $('.logo').css('width', '900px');
       $('.logo').css('height','900px');
       $('header').css('height','900px');
+      $('header').css('opacity','1');
     }
   });
