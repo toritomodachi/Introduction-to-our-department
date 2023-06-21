70b81c7045219696c6c5378edbba2c0e9af7ee01
toritomodachi <tanncheonn@gmail.com>
2023-06-21 11:28:10 +0900

    아 진짜 머리 안돌아가네

--- script/server.js
+++ script/server.js
@@ -1,5 +1,7 @@
+const count=0;
+if(count == 0){
 var prevScroll = pageYOffset;
-        window.addEventListener("scroll", () => prevScroll = pageYOffset);
+        //window.addEventListener("scroll", () => prevScroll = pageYOffset);
         window.addEventListener("click", (e) => {
         if (pageYOffset !== prevScroll) return;
         e.preventDefault();
@@ -28,10 +30,31 @@ $(window).on('scroll', function(){
     $('.logo2').css('opacity','0');
     $('.logo').css('display','block');
     }
-    if(window.scrollY > 1900 && window.scrollY < 2800){
-      window.addEventListener('scroll', function(){
-        console.log( window.scrollY )
-      });
-    }
   });
+});
+conut++;
+}
+$(window).on('scroll', function(){
+  if (window.scrollY > 100) {
+    $('.logo').css('width', '100px');
+    $('.logo').css('height','100px');
+    $('header').css('height','100px');
+    $('header').css('opacity','0.5');
+  }
+  else{
+      $('.logo').css('width', '900px');
+    $('.logo').css('height','900px');
+    $('header').css('height','900px');
+    $('header').css('opacity','1');
+  }
+  if (window.scrollY > 1060) {
+    $('.Mainmenu').css('display','inline-block');
+    $('.logo2').css('opacity','1');
+    $('.logo').css('display','none');
+  }
+  else{
+  $('.Mainmenu').css('display','none');
+  $('.logo2').css('opacity','0');
+  $('.logo').css('display','block');
+  }
 });
